ff39e720f0279c6f623339c64fcb90ace0036c53
MarceloGatto <marcelogatto1@gmail.com>
2020-12-10 14:43:14 -0300

    Minha primeira pág html

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <h1>Estou aprendendo como subir um sistema em produção<h1>
 <h1> git add . <h1>
-<h1> git commit - "Meu primeiro deploy" <h1>
+<h1> git commit -m "Meu primeiro deploy" <h1>
+<h1> git push <h1>
